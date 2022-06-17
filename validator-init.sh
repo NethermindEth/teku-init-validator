4033ae00bbaffe0e098176888925467e8f270c67
Miguel Tenorio <46824157+AntiD2ta@users.noreply.github.com>
2022-06-17 00:44:38 -0400

    Create validator-init.sh

---
+++ validator-init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+if [[ "$(ls /data/validators/keys)" ]]; then
+    echo "Keystore directory is not empty. Skipping." && exit 0
+else
+    mkdir -p /data/validator/keys /data/validator/passwords
+    cd /keystore/validator_keys
+    for key in *; do
+        FILENAME=`echo ${key} | sed 's/.json//g'`
+        cp ${key} "/data/validator/keys/${FILENAME}.json"
+        cp ../keystore_password.txt "/data/validator/passwords/${FILENAME}.txt"
+        echo "Copying ${key}"
+    done
+fi
